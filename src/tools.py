61b61a5dfee8107572165d1fff40b4ed11cfe33e
Rajesh Roshan <Rajesh.Roshan@amantyatech.com>
2026-09-13 14:50:23 +0530

    completed one operation

--- src/tools.py
+++ src/tools.py
@@ -14,8 +14,3 @@ def calculator(a: float, b: float, operation: str) -> float:
         return a / b
 
     raise ValueError(f"Unknown operation: {operation}")
-
-
-# if __name__ == "__main__":
-#     result = calculator(25, 48, "multiply")
-#     print(result)
